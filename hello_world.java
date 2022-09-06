b1abbd087f40bd9769555a8a2e3cc67357de955d
manisha <manisha.verma23@gmail.com>
2022-09-06 17:06:46 +0530

    Java hello_World program

---
+++ hello_world.java
@@ -0,0 +1,8 @@
+//  My First Java Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!!"); 
+        System.out.println("WELCOME YOU ALL!!"); 
+    }
+}
